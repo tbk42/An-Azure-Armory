e019451da36e69c2be8a248722810a36382704d2
Eric Reinhardt <eric@blueknight.org>
2025-06-17 19:14:36 -0400

    ?Update to directory detection routine

--- library.sh
+++ library.sh
@@ -42,14 +42,27 @@
 #  - sec2dhms
 # -----------------------------------------------------------------
 # Script name, real name, real dir, real path
-# script_real_path=$(realpath "${BASH_SOURCE:-$0}")
-# script_real_dir=$(dirname "$script_real_path")
-# script_real_name="$(echo "$script_real_path" | rev | cut -d/ -f1 | rev)";
-# script_local_name="$(echo "$0" | rev | cut -d/ -f1 | rev)";
-# source "${script_real_dir}/color.sh"
-# source "${script_real_dir}/cryptography.sh"
-# source "${script_real_dir}/file_system.sh"
-# source "${script_real_dir}/general.sh"
-# source "${script_real_dir}/math.sh"
-# source "${script_real_dir}/string.sh"
-# source "${script_real_dir}/time.sh"
+# 
+#   script_local      = "${BASH_SOURCE:-${0}}"            /unified/projects/deploy-manager/the_script.sh
+#   script_local_path = "$(dirname  "${script_local}")"   /unified/projects/deploy-manager
+#   script_local_name = "$(basename "${script_local}")"   the_script.sh
+#   script_real       = "$(realpath "${script_local}")"   /unified/git/tbk42/deploy-manager/deploy-manager.sh
+#   script_real_path  = "$(dirname  "${script_real}")"    /unified/git/tbk42/deploy-manager
+# > script_real_path  = "$(dirname  "$(realpath "${BASH_SOURCE:-${0}}")")"
+#   script_real_name  = "$(basename "${script_real}")"    deploy-manager.sh
+# 
+# echo -e "script_local: ${script_local}"
+# echo -e "script_local_name: ${script_local_name}"
+# echo -e "script_local_path: ${script_local_path}"
+# echo -e "script_real: ${script_real_full_path}"
+# echo -e "script_real_path: ${script_real_path}"
+# echo -e "script_real_name: ${script_real_name}"
+# 
+# > source "${script_real_path}/An-Azure-Armory/color.sh"
+#   source "${script_real_path}/color.sh"
+#   source "${script_real_path}/cryptography.sh"
+#   source "${script_real_path}/file_system.sh"
+#   source "${script_real_path}/general.sh"
+#   source "${script_real_path}/math.sh"
+#   source "${script_real_path}/string.sh"
+#   source "${script_real_path}/time.sh"
