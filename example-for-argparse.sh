74a92bd64abe95124499e9d2b4715dc337166d5b
Eric Reinhardt <eric@blueboxcars.dev>
2026-05-22 23:38:32 -0400

    example-for-argparse.sh: Add usage example for argparse library

---
+++ example-for-argparse.sh
@@ -0,0 +1,210 @@
+#!/usr/bin/env bash
+
+# source ./argparse.sh
+
+#!/usr/bin/env bash
+
+# ----------------------------
+# Defaults (can override)
+# ----------------------------
+ARG_DELAY=5
+ARG_SLEEP=5
+
+
+# ----------------------------
+# Helpers
+# ----------------------------
+is_value() {
+    [[ "$1" =~ ^-?[0-9]+$ ]]
+}
+
+
+# ----------------------------
+# Usage Generator
+# ----------------------------
+arg_usage() {
+
+    cat <<EOF
+Usage: $0 [options]
+
+Options:
+  -b, --background        Run in background
+  -d, --delay[=N]         Delay (default: $ARG_DELAY)
+  -p, --procs=N           Process refresh
+  -s, --sleep[=N]         Sleep time (default: $ARG_SLEEP)
+  -k, --kill              Kill old
+  -n, --no-kill           Do not kill
+  -h, --help              Show help
+EOF
+}
+
+
+# ----------------------------
+# Normalize
+# ----------------------------
+arg_normalize() {
+
+    local normalized=()
+
+    while [[ $# -gt 0 ]]; do
+        local arg="$1"
+        shift
+
+        case "$arg" in
+
+            -b|--background)
+                normalized+=(--background)
+                ;;
+
+            -d|--delay)
+                if [[ $# -gt 0 ]] && is_value "$1"; then
+                    normalized+=(--delay="$1")
+                    shift
+                else
+                    normalized+=(--delay="$ARG_DELAY")
+                fi
+                ;;
+
+            -d=*|--delay=*)
+                normalized+=(--delay="${arg#*=}")
+                ;;
+
+            -p|--procs)
+                if [[ $# -gt 0 ]] && is_value "$1"; then
+                    normalized+=(--procs="$1")
+                    shift
+                else
+                    printf "%s
+" ""Missing value for --procs" >&2"
+                    return 1
+                fi
+                ;;
+
+            -p=*|--procs=*)
+                normalized+=(--procs="${arg#*=}")
+                ;;
+
+            -s|--sleep)
+                if [[ $# -gt 0 ]] && is_value "$1"; then
+                    normalized+=(--sleep="$1")
+                    shift
+                else
+                    normalized+=(--sleep="$ARG_SLEEP")
+                fi
+                ;;
+
+            -s=*|--sleep=*)
+                normalized+=(--sleep="${arg#*=}")
+                ;;
+
+            -k|--kill|--killold|--kill-old)
+                normalized+=(--kill)
+                ;;
+
+            -n|--nokill|--no-kill)
+                normalized+=(--no-kill)
+                ;;
+
+            -h|--help)
+                normalized+=(--help)
+                ;;
+
+            *)
+                printf "%s
+" ""Unknown option: $arg" >&2"
+                return 1
+                ;;
+        esac
+    done
+
+    printf '%s\n' "${normalized[@]}"
+}
+
+# ---END OF ARGPARSE LIBRARY--------------------------------------------------------------------------------
+
+
+# ----------------------------
+# Argument Handler
+# ----------------------------
+arguments() {
+
+    # Defaults (local to app)
+    DELAY="$ARG_DELAY"
+    SLEEP="$ARG_SLEEP"
+    KILL=true
+    PROCS=""
+
+    # Normalize
+    mapfile -t NORMALIZED < <(arg_normalize "$@") || return 1
+
+    set -- "${NORMALIZED[@]}"
+
+
+    # Map to short flags
+    local mapped=()
+
+    for arg in "$@"; do
+        case "$arg" in
+
+            --background) mapped+=(-b) ;;
+            --delay=*)    mapped+=(-d "${arg#*=}") ;;
+            --procs=*)    mapped+=(-p "${arg#*=}") ;;
+            --sleep=*)    mapped+=(-s "${arg#*=}") ;;
+            --kill)       mapped+=(-k) ;;
+            --no-kill)    mapped+=(-n) ;;
+            --help)       mapped+=(-h) ;;
+
+        esac
+    done
+
+    set -- "${mapped[@]}"
+
+
+    # Parse
+    while getopts ":bd:p:s:knh" opt; do
+        case "$opt" in
+
+            b) DELAY=-1 ;;
+            d) DELAY="$OPTARG" ;;
+            p) PROCS="$OPTARG" ;;
+            s) SLEEP="$OPTARG" ;;
+            k) KILL=true ;;
+            n) KILL=false ;;
+            h) arg_usage; exit 0 ;;
+
+            :)
+                printf "%s
+" ""Option -$OPTARG needs value" >&2"
+                return 1
+                ;;
+
+            \?)
+                printf "%s
+" ""Invalid option -$OPTARG" >&2"
+                return 1
+                ;;
+        esac
+    done
+}
+
+
+# ----------------------------
+# Main
+# ----------------------------
+main() {
+
+    arguments "$@" || exit 1
+
+    printf "%s
+" ""Delay: $DELAY""
+    printf "%s
+" ""Sleep: $SLEEP""
+    printf "%s
+" ""Kill:  $KILL""
+    printf "%s
+" ""Procs: $PROCS""
+
+    # Your real logic here
+}
+
+main "$@"
